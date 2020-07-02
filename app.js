1dfc34d59efe276a2399e891dbd1804a31a61753
Jordy51 <adityakumawat51@gmail.com>
2020-07-03 02:44:17 +0530

    Route problem Solved

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use((req, res, next ) => {
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/note', require('./routes/note'))
 
 const PORT = process.env.PORT || 5000;
 
